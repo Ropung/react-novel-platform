08324e68bb9c1e8586ef3413c677a574393101f7
Gihun <563710@naver.com>
2022-12-26 17:18:23 +0900

    chore(home): style work

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -69,117 +69,119 @@ const HomePage = () => {
           ))}
         </div>
         {/* 테마 별 작품 목록 */}
-        <ul className="w-full flex flex-col gap-8">
-          {/* 카테고리별나누기 map 함수로 */}
-          <li className="flex flex-col gap-4">
-            {/* Title */}
-            <div className="w-full flex flex-row justify-between items-center">
-              <p className="text-4xl font-bold">오늘의 인기작</p>
-              <div className="flex flex-row gap-2 justify-end text-4xl">
-                <BsArrowLeftSquareFill className="cursor-pointer" />
-                <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+        <div className="flex flex-col gap-32">
+          <ul className="w-full flex flex-col gap-2">
+            {/* 카테고리별나누기 map 함수로 */}
+            <li className="flex flex-col gap-4">
+              {/* Title */}
+              <div className="w-full flex flex-row justify-between items-center">
+                <p className="text-4xl font-bold">오늘의 인기작</p>
+                <div className="flex flex-row gap-2 justify-end text-4xl">
+                  <BsArrowLeftSquareFill className="cursor-pointer" />
+                  <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+                </div>
               </div>
-            </div>
-            {/* Novel List */}
-            <ul className="w-full flex flex-row gap-2 overflow-hidden">
-              {novelList.map((item) => (
-                <li key={item.novelid} className="w-[20%]">
-                  <div className="flex flex-col gap-4">
-                    <img
-                      className="w-full px-2"
-                      src="http://image.yes24.com/goods/106211628/XL"
-                      alt=""
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-        {/* 테마 별 작품 목록 */}
-        <ul className="w-full flex flex-col gap-8">
-          {/* 카테고리별나누기 map 함수로 */}
-          <li className="flex flex-col gap-4">
-            {/* Title */}
-            <div className="w-full flex flex-row justify-between items-center">
-              <p className="text-4xl font-bold">오늘의 인기작</p>
-              <div className="flex flex-row gap-2 justify-end text-4xl">
-                <BsArrowLeftSquareFill className="cursor-pointer" />
-                <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+              {/* Novel List */}
+              <ul className="w-full flex flex-row gap-2 overflow-hidden">
+                {novelList.map((item) => (
+                  <li key={item.novelid} className="w-[20%]">
+                    <div className="flex flex-col gap-4">
+                      <img
+                        className="w-full px-2"
+                        src="http://image.yes24.com/goods/106211628/XL"
+                        alt=""
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+          {/* 테마 별 작품 목록 */}
+          <ul className="w-full flex flex-col gap-8">
+            {/* 카테고리별나누기 map 함수로 */}
+            <li className="flex flex-col gap-4">
+              {/* Title */}
+              <div className="w-full flex flex-row justify-between items-center">
+                <p className="text-4xl font-bold">오늘의 인기작</p>
+                <div className="flex flex-row gap-2 justify-end text-4xl">
+                  <BsArrowLeftSquareFill className="cursor-pointer" />
+                  <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+                </div>
               </div>
-            </div>
-            {/* Novel List */}
-            <ul className="w-full flex flex-row gap-2 overflow-hidden">
-              {novelList.map((item) => (
-                <li key={item.novelid} className="w-[20%]">
-                  <div className="flex flex-col gap-4">
-                    <img
-                      className="w-full px-2"
-                      src="http://image.yes24.com/goods/106211628/XL"
-                      alt=""
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-        {/* 테마 별 작품 목록 */}
-        <ul className="w-full flex flex-col gap-8">
-          {/* 카테고리별나누기 map 함수로 */}
-          <li className="flex flex-col gap-4">
-            {/* Title */}
-            <div className="w-full flex flex-row justify-between items-center">
-              <p className="text-4xl font-bold">오늘의 인기작</p>
-              <div className="flex flex-row gap-2 justify-end text-4xl">
-                <BsArrowLeftSquareFill className="cursor-pointer" />
-                <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+              {/* Novel List */}
+              <ul className="w-full flex flex-row gap-2 overflow-hidden">
+                {novelList.map((item) => (
+                  <li key={item.novelid} className="w-[20%]">
+                    <div className="flex flex-col gap-4">
+                      <img
+                        className="w-full px-2"
+                        src="http://image.yes24.com/goods/106211628/XL"
+                        alt=""
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+          {/* 테마 별 작품 목록 */}
+          <ul className="w-full flex flex-col gap-8">
+            {/* 카테고리별나누기 map 함수로 */}
+            <li className="flex flex-col gap-4">
+              {/* Title */}
+              <div className="w-full flex flex-row justify-between items-center">
+                <p className="text-4xl font-bold">오늘의 인기작</p>
+                <div className="flex flex-row gap-2 justify-end text-4xl">
+                  <BsArrowLeftSquareFill className="cursor-pointer" />
+                  <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+                </div>
               </div>
-            </div>
-            {/* Novel List */}
-            <ul className="w-full flex flex-row gap-2 overflow-hidden">
-              {novelList.map((item) => (
-                <li key={item.novelid} className="w-[20%]">
-                  <div className="flex flex-col gap-4">
-                    <img
-                      className="w-full px-2"
-                      src="http://image.yes24.com/goods/106211628/XL"
-                      alt=""
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-        {/* 테마 별 작품 목록 */}
-        <ul className="w-full flex flex-col gap-8">
-          {/* 카테고리별나누기 map 함수로 */}
-          <li className="flex flex-col gap-4">
-            {/* Title */}
-            <div className="w-full flex flex-row justify-between items-center">
-              <p className="text-4xl font-bold">오늘의 인기작</p>
-              <div className="flex flex-row gap-2 justify-end text-4xl">
-                <BsArrowLeftSquareFill className="cursor-pointer" />
-                <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+              {/* Novel List */}
+              <ul className="w-full flex flex-row gap-2 overflow-hidden">
+                {novelList.map((item) => (
+                  <li key={item.novelid} className="w-[20%]">
+                    <div className="flex flex-col gap-4">
+                      <img
+                        className="w-full px-2"
+                        src="http://image.yes24.com/goods/106211628/XL"
+                        alt=""
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+          {/* 테마 별 작품 목록 */}
+          <ul className="w-full flex flex-col gap-8">
+            {/* 카테고리별나누기 map 함수로 */}
+            <li className="flex flex-col gap-4">
+              {/* Title */}
+              <div className="w-full flex flex-row justify-between items-center">
+                <p className="text-4xl font-bold">오늘의 인기작</p>
+                <div className="flex flex-row gap-2 justify-end text-4xl">
+                  <BsArrowLeftSquareFill className="cursor-pointer" />
+                  <BsArrowLeftSquareFill className=" cursor-pointer rotate-180" />
+                </div>
               </div>
-            </div>
-            {/* Novel List */}
-            <ul className="w-full flex flex-row gap-2 overflow-hidden">
-              {novelList.map((item) => (
-                <li key={item.novelid} className="w-[20%]">
-                  <div className="flex flex-col gap-4">
-                    <img
-                      className="w-full px-2"
-                      src="http://image.yes24.com/goods/106211628/XL"
-                      alt=""
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
+              {/* Novel List */}
+              <ul className="w-full flex flex-row gap-2 overflow-hidden">
+                {novelList.map((item) => (
+                  <li key={item.novelid} className="w-[20%]">
+                    <div className="flex flex-col gap-4">
+                      <img
+                        className="w-full px-2"
+                        src="http://image.yes24.com/goods/106211628/XL"
+                        alt=""
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   );
